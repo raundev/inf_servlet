9d921abefebd2c5a5135be738d0d3924e113eb24
raundev <lbil7641@gmail.com>
2023-04-27 21:50:24 +0900

    @ServletComponentScan 을 통한 서블릿 자동 등록 application.properties에 속성?추가하여 http로그 확인하기 월컴페이지(index.html) 페이지 추가

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,35 @@
+package hello.servlet.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    /**
+     * HTTP 요청이 오면 was가 HttpServletRequest 객체와 HttpServletResponse 객체를 만들어 서블릿으로 던져준다.
+     * @param request  the {@link HttpServletRequest} object that contains the request the client made of the servlet
+     * @param response the {@link HttpServletResponse} object that contains the response the servlet returns to the client
+     *
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username );//HHTP 메시지 BODY에 들어감
+    }
+}
