f6c79b3faf3a77fdf52c2243ff3c1662c3691ba7
raundev <lbil7641@gmail.com>
2023-04-27 23:33:03 +0900

    HttpServletRequest request 에 존재하는 정보 확인하기

--- src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -1,19 +1,17 @@
 package hello.servlet.basic.request;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @WebServlet(name = "requestHeaderServlet", urlPatterns = "/request-header")
 public class RequestHeaderServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+          {
         printStartLine(request);
         printHeaders(request);
         printHeaderUtils(request);
