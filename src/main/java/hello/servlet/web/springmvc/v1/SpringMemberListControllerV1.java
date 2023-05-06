3d51a73e70a6d5524c8e1ff5a25e76829ec10e46
raundev <lbil7641@gmail.com>
2023-05-06 14:58:39 +0900

    Spring MVC 시작하기

---
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberListControllerV1.java
@@ -0,0 +1,25 @@
+package hello.servlet.web.springmvc.v1;
+
+import hello.servlet.domian.member.Member;
+import hello.servlet.domian.member.MemberRepository;
+import hello.servlet.web.frontcontroller.ModelView;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SpringMemberListControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+    @RequestMapping("/springmvc/v1/members")
+    public ModelAndView process(HttpServletRequest request) {
+        List<Member> members = memberRepository.findAll();
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject ("members", members);
+
+        return mv;
+    }
+}
---
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberSaveControllerV1.java
@@ -0,0 +1,30 @@
+package hello.servlet.web.springmvc.v1;
+
+import hello.servlet.domian.member.Member;
+import hello.servlet.domian.member.MemberRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Map;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SpringMemberSaveControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members/save")
+    public ModelAndView process(HttpServletRequest request) {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        ModelAndView mv = new ModelAndView("save-result");
+        //mv.getModel().put("member", member);
+        mv.addObject("member",member);
+
+        return mv;
+    }
+}
